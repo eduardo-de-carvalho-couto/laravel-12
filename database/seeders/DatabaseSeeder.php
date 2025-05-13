39e00a78316b3d08839b5c7ad9f690dd05ee2118
Eduardo Couto <eduardo@lemarq.com.br>
2025-05-13 04:24:56 -0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Category::factory(20)->create();
+
         \App\Models\User::factory(500)->create()->each(function ($user) {
-            $posts = \App\Models\Post::factory(5)->make();
+            $posts = \App\Models\Post::factory(5)->make(); // 5 posts por usuário
             $user->posts()->saveMany($posts);
 
             $posts->each(function ($post) {
                 $post->comments()->saveMany(
-                    \App\Models\Comment::factory(3)->make()
+                    \App\Models\Comment::factory(3)->make() // 3 comentários por post
                 );
 
                 $categoryIds = \App\Models\Category::inRandomOrder()->take(rand(1, 3))->pluck('id');
